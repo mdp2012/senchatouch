32ca8118eed13f2ed2642941276e78e9e77d0f5f
Robin Mueller <robinmueller@me.com>
2012-09-26 16:32:07 +0200

    Created Sencha Module

---
+++ magento/app/code/local/Senchahack/Sencha/Helper/Data.php
@@ -0,0 +1,7 @@
+<?php
+
+class Senchahack_Sencha_Helper_Data extends Mage_Core_Helper_Abstract
+{
+   
+}
+?>
---
+++ magento/app/code/local/Senchahack/Sencha/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+class Senchahack_Sencha_IndexController extends Mage_Core_Controller_Front_Action {
+
+    public function indexAction() {
+        
+        die('Sencha');
+    }
+
+    
+    
+
+
+}
+
