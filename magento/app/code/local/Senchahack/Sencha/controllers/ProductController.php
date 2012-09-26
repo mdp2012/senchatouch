f52428d7fc45ea835f3851877e9c33826227e05c
Jürgen Vogel <mail@jv-webentwicklung.de>
2012-09-26 17:27:50 +0200

    added frontend layout handle to access /sencha/

--- magento/app/code/local/Senchahack/Sencha/controllers/ProductController.php
+++ magento/app/code/local/Senchahack/Sencha/controllers/ProductController.php
@@ -2,13 +2,13 @@
 
 class Senchahack_Sencha_ProductController extends Mage_Core_Controller_Front_Action
 {
-
-    public function indexAction()
+    public function getAction()
     {
         $productId  = (int)$this->getRequest()->getParam('id');
         $product    = Mage::getModel('catalog/product')->load($productId);
 
         if (!$product) {
+            echo 'no product found';
             return;
         }
 
