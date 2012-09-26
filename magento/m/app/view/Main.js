5758427d21c57bffc1144796a532732bb5a78b2f
Arush Sehgal <arush@ilovebrands.net>
2012-09-26 18:17:03 +0100

    also added touchstyle example app in m2

--- magento/m/app/view/Main.js
+++ magento/m/app/view/Main.js
@@ -2,14 +2,15 @@ Ext.define("MagentoSenchaApp.view.Main", {
     extend: 'Ext.tab.Panel',
     requires: [
         'Ext.TitleBar',
-        'Ext.Video'
+        'Ext.Video',
+        'MagentoSenchaApp.view.Categories'
     ],
     config: {
         tabBarPosition: 'bottom',
 
         items: [
             {
-                title: 'Welcome',
+                title: 'Home',
                 iconCls: 'home',
 
                 styleHtmlContent: true,
@@ -18,17 +19,17 @@ Ext.define("MagentoSenchaApp.view.Main", {
                 items: {
                     docked: 'top',
                     xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
+                    title: 'Welcome to Magento Store!'
                 },
 
                 html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
+                    "this is our homepage with lots of awesome stuff in it.",
                     "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
+                    "hello this is a test"
                 ].join("")
             },
             {
-                title: 'Get Started',
+                title: 'Catalog',
                 iconCls: 'action',
 
                 items: [
@@ -41,6 +42,23 @@ Ext.define("MagentoSenchaApp.view.Main", {
                         xtype: 'video',
                         url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
                         posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                    }
+                ]
+            },
+            {
+                title: 'Cart',
+                iconCls: 'action',
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Shopping Cart'
+                    },
+                    {
+                        html: [
+                            "this is the shopping cart"
+                        ].join("")
                     }
                 ]
             }
